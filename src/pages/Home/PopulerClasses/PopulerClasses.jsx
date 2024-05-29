a425f0e25917dc513d97a6c6c6f3ea60260b731d
Mahmudul-hasan-dev <contact.mahmudul.h@gmail.com>
2024-05-29 23:21:41 +0600

    popular class

--- src/pages/Home/PopulerClasses/PopulerClasses.jsx
+++ src/pages/Home/PopulerClasses/PopulerClasses.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import useClass from '../../../hooks/useClass';
+import { Card,  Container} from 'react-bootstrap';
+import '../Home/Home.css'
 
 const PopulerClasses = () => {
+const [classes] = useClass();
+const popularClasses = classes;
     return (
-        <div>
-            class
+        <div className='cards-container'>
+    
+          
+            {
+                popularClasses.map( pc=> 
+                  <div key={pc._id}>
+                    <Container fluid>
+                    <Card  >
+                        <Card.Img variant="top" src={pc.image} height={'200px'} />
+                        <Card.Body>
+                        <Card.Title className='fs-1'>{pc.name}</Card.Title>
+                        <Card.Text>
+                            <h4>Enrolled Students: {pc.students}</h4>
+                        </Card.Text>
+                        
+                        </Card.Body>
+                     </Card>
+                    </Container>
+                  </div>
+                   
+               
+                    )
+            }
+          
         </div>
     );
 };
