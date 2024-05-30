3714e9311a45a1710d507b62b18b7a12b455e080
Mahmudul-hasan-dev <contact.mahmudul.h@gmail.com>
2024-05-30 10:23:04 +0600

    backgroud cololr

--- src/pages/Classes/AllClasses.jsx
+++ src/pages/Classes/AllClasses.jsx
@@ -12,9 +12,9 @@ const AllClasses = () => {
                 classes.map( c=> 
                   <div key={c._id}>
                     <Container fluid>
-                        { c.seats !== 0 &&
+                        { c.seats !== 0 ? 
                             
-                                <Card className='bg-danger' >
+                                <Card >
                                 <Card.Img variant="top" src={c.image} height={'200px'} />
                                 <Card.Body>
                                 <Card.Title className='fs-1' >{c.name}</Card.Title>
@@ -25,9 +25,21 @@ const AllClasses = () => {
                                 <Card.Footer>
                                   <Button variant="primary">Select</Button>
                                 </Card.Footer>
-                             </Card>
-                            
-                        }
+                             </Card> :
+                             <Card className='bg-danger text-white'>
+                             <Card.Img variant="top" src={c.image} height={'200px'} />
+                             <Card.Body>
+                             <Card.Title className='fs-1' >{c.name}</Card.Title>
+                             <h5 className= 'fs-bold'> Instructor: {c.instructor_name}</h5>
+                             <h3> Available Seats: {c.seats}</h3>
+                             <h4> Price: ${c.price} </h4>
+                             </Card.Body>
+                             <Card.Footer>
+                               <Button variant="primary" disabled>Select</Button>
+                             </Card.Footer>
+                          </Card>
+                  }
+                        
                      
                     
                     </Container>
