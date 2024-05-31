a18a82eff9e44df4340274ec856a1505316c305c
Mahmudul-hasan-dev <contact.mahmudul.h@gmail.com>
2024-05-31 19:27:54 +0600

    add class

--- src/pages/Dashboard/AddClass/AddClass.jsx
+++ src/pages/Dashboard/AddClass/AddClass.jsx
@@ -9,7 +9,7 @@ import Swal from 'sweetalert2';
 
 
 const image_hosting_token = import.meta.env.VITE_IMAGE_UPLOAD_TOKEN;
-const image_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${image_hosting_token}`
+const image_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hosting_token}`
 
 const AddClass = () => {
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
